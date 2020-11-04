de9d9509dad1d7d839fa183b2db5f69228fef98c
MATTHEW\Matthew <matthew@stair.biz>
2020-11-04 21:58:12 +0800

    orange background fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,14 +16,17 @@ export class HomeComponent implements OnInit {
 
   onWindowScroll(e): void {
     const element = document.querySelector('.navbar');
+    const navToggler = document.getElementById('navbarNav');
     if (window.pageYOffset > element.clientHeight) {
       element.classList.add('bg-dark');
     } else {
+      navToggler.classList.add('collapse');
       element.classList.remove('bg-dark');
     }
   }
     goToElement(el: HTMLElement): void {
       el.scrollIntoView({behavior: 'smooth'});
+      this.toggle();
   }
 
   toggle(): void {
